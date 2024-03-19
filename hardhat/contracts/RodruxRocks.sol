19e475f9e9b4a77cf510904bd0a8b17920c78eb5
Rodrigo Goitia <rodrigojgq@gmail.com>
2024-03-18 21:12:43 -0300

    Base contract for RodruxRocks tokens

--- hardhat/contracts/RodruxRocks.sol
+++ hardhat/contracts/RodruxRocks.sol
@@ -3,20 +3,99 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "./RodruxRocksDNA.sol";
 
-contract RodruxRocks is ERC721, ERC721Enumerable {
-  uint256 public maxSupply;
+contract RodruxRocks is ERC721, ERC721Enumerable, RodruxRockDNA {
+    uint256 public maxSupply;
 
+    mapping (uint256 => uint256) tokenDNA;
 
-   constructor(uint256 _maxSupply) ERC721("RodruxRocks", "RXRKS") {
-    maxSupply = _maxSupply;
-   }
-
-    function mint() public{
-      uint256 counter = totalSupply();
-      require(counter < maxSupply, "No more RodruxRocks available");
-      _safeMint(msg.sender, counter);
+    constructor(uint256 _maxSupply) ERC721("RodruxRocks", "RXRKS") {
+        maxSupply = _maxSupply;
     }
+
+    function mint() public {
+        uint256 counter = totalSupply();
+        require(counter < maxSupply, "No more RodruxRocks available");
+
+        tokenDNA[counter] = deterministicPseudoRandomDNA(counter);
+        _safeMint(msg.sender, counter);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
+
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
+
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        require(
+            tokenId < totalSupply(),
+            "ERC721 Metadata: URI query for nonexistent token"
+        );
+
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
+
+        string memory jsonURI = Base64.encode(
+            abi.encode(
+                '{ "name": "RodruxRocks #',
+                tokenId,
+                '", "description": "RodruxRocks are randomized Rock Avataaars stored on chain to learn DApps development", "image": "',
+                image,
+                '"attributes": []'
+                '"}'
+            )
+        );
+
+        return string(abi.encode("data:application/json;base64,", jsonURI));
+    }
+
     // The following functions are overrides required by Solidity.
 
     function _update(
