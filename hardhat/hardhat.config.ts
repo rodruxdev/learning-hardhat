2f10f3eedf00e5040983ec5d57bfb8f8bfd65c28
Rodrigo Goitia <rgoitia@ultragrupo.com>
2024-03-18 21:12:43 -0300

    Base contract for RodruxRocks tokens

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,8 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import { vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const projectId = vars.get("INFURA_PROJECT_ID");
-const privateKey = vars.get("PRIVATE_KEY");
+// const projectId = vars.get("INFURA_PROJECT_ID");
+// const privateKey = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
