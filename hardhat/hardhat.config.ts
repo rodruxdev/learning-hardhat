cc5d95d88d6676480a562daf2400b540ff67e2c7
Rodrigo Goitia <rodrigojgq@gmail.com>
2024-03-19 17:54:37 -0300

    Change config to verify contract in sepolia

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,17 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import { vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-// const projectId = vars.get("INFURA_PROJECT_ID");
-// const privateKey = vars.get("PRIVATE_KEY");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  // networks: {
-  //   sepolia: {
-  //     url: `https://sepolia.infura.io/v3/${projectId}`,
-  //     accounts: [privateKey]
-  //   },
-  // }
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  }
 };
 
 export default config;
