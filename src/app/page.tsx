b6c026028f4b84d1963754affce62b4bc37da2a2
Rodrigo Goitia <rgoitia@ultragrupo.com>
2024-03-21 20:00:42 -0300

    Complete connect and disconnect interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Header></Header>
     </main>
   );
